d24fab5f17be19c0629a9816b3e9c8565c470d73
Gabriele Picco <piccogabriele@gmail.com>
2015-12-17 09:39:17 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	HospitalOS/.metadata/.log
    #	HospitalOS/HotspitalOS/paziente.c

--- HospitalOS/HotspitalOS/paziente.c
+++ HospitalOS/HotspitalOS/paziente.c
@@ -4,6 +4,14 @@
  * Paziente
  */
 int main(int argc,char** argv){
+<<<<<<< Updated upstream
 	printf("\nSono il paziente x o y ?!?!?!?!? !\n");
+=======
+<<<<<<< HEAD
+	printf("\nSono il paziente XYz !\n");
+=======
+	printf("\nSono il paziente x o y ?!?!?!?!? !\n");
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 	return 0;
 }
